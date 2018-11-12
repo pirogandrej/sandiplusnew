c190da28e8c18802b2731e09b92d302119310f91
pirogandrej <pirog.andrej@gmail.com>
2018-11-12 17:49:59 +0200

    three.js geometry figure opacity backside

--- public/js/custom-three.js
+++ public/js/custom-three.js
@@ -34,68 +34,147 @@ function func_threejs_init() {
             case 'sphere':
                 // arrayFigure[i].geometry = new THREE.SphereGeometry( 100, 8, 6 );
 
+                // arrayFigure[i].scene = new THREE.Scene();
+                // arrayFigure[i].scene.background = new THREE.Color( bgColorFigure );
+                // arrayFigure[i].camera = new THREE.PerspectiveCamera(40, widthFigure/heightFigure , 0.1, 1000);
+                // arrayFigure[i].camera.position.set( 0, 0, 350);
+                // arrayFigure[i].renderer = new THREE.WebGLRenderer({ antialias: true });
+                // arrayFigure[i].renderer.setPixelRatio( window.devicePixelRatio );
+                // arrayFigure[i].renderer.setSize( widthFigure - paddingFigure*2 ,heightFigure - paddingFigure*2);
+                // arrayFigure[i].container = document.getElementById(arrayFigure[i].name);
+                // arrayFigure[i].container.appendChild( arrayFigure[i].renderer.domElement );
+                //
+                // arrayFigure[i].scene.add( new THREE.AmbientLight( 0xaaaaaa ) );
+                // arrayFigure[i].light = new THREE.PointLight( 0x000000, 1 );
+                // arrayFigure[i].camera.add( arrayFigure[i].light );
+                //
+                // arrayFigure[i].geometry.vertices = new THREE.DodecahedronGeometry( 100 ).vertices;
+                // arrayFigure[i].pointsMaterial = new THREE.PointsMaterial( {
+                //     color: 0x999999,
+                //     size: 16,
+                //     alphaTest: 0.5
+                // } );
+                // arrayFigure[i].pointsGeometry = new THREE.BufferGeometry().setFromPoints( arrayFigure[i].geometry.vertices );
+                // arrayFigure[i].points = new THREE.Points( arrayFigure[i].pointsGeometry, arrayFigure[i].pointsMaterial );
+                // arrayFigure[i].scene.add( arrayFigure[i].points );
+                //
+                // arrayFigure[i].material = new THREE.MeshLambertMaterial( {
+                //     color: 0x00ee00,
+                //     opacity: 0.1,
+                //     transparent: true,
+                //     wireframe: true
+                // } );
+                //
+                // arrayFigure[i].geometry = new THREE.ConvexBufferGeometry( arrayFigure[i].geometry.vertices );
+                //
+                // arrayFigure[i].mesh = new THREE.Mesh(arrayFigure[i].geometry, arrayFigure[i].material);
+                // arrayFigure[i].mesh.material.side = THREE.BackSide;
+                // arrayFigure[i].mesh.renderOrder = 0;
+                // arrayFigure[i].scene.add( arrayFigure[i].mesh );
+                //
+                // arrayFigure[i].mesh = new THREE.Mesh( arrayFigure[i].geometry, arrayFigure[i].material.clone() );
+                // arrayFigure[i].mesh.material.side = THREE.FrontSide;
+                // arrayFigure[i].mesh.renderOrder = 1;
+                // arrayFigure[i].scene.add( arrayFigure[i].mesh );
+
+
+
+
+
+
+
+
+
+
+
+
+
                 arrayFigure[i].scene = new THREE.Scene();
                 arrayFigure[i].scene.background = new THREE.Color( bgColorFigure );
                 arrayFigure[i].camera = new THREE.PerspectiveCamera(40, widthFigure/heightFigure , 0.1, 1000);
-                arrayFigure[i].camera.position.set( 0, 0, 350);
+                arrayFigure[i].camera.position.set( 0, 0, 50);
                 arrayFigure[i].renderer = new THREE.WebGLRenderer({ antialias: true });
-                arrayFigure[i].renderer.setPixelRatio( window.devicePixelRatio );
                 arrayFigure[i].renderer.setSize( widthFigure - paddingFigure*2 ,heightFigure - paddingFigure*2);
                 arrayFigure[i].container = document.getElementById(arrayFigure[i].name);
                 arrayFigure[i].container.appendChild( arrayFigure[i].renderer.domElement );
 
-                arrayFigure[i].scene.add( new THREE.AmbientLight( 0xaaaaaa ) );
-                arrayFigure[i].light = new THREE.PointLight( 0x000000, 1 );
-                arrayFigure[i].camera.add( arrayFigure[i].light );
+                arrayFigure[i].controls = new THREE.OrbitControls(arrayFigure[i].camera, arrayFigure[i].renderer.domElement);
 
-                arrayFigure[i].geometry.vertices = new THREE.DodecahedronGeometry( 100 ).vertices;
-                arrayFigure[i].pointsMaterial = new THREE.PointsMaterial( {
-                    color: 0x999999,
-                    size: 6,
-                    alphaTest: 0.5
-                } );
-                arrayFigure[i].pointsGeometry = new THREE.BufferGeometry().setFromPoints( arrayFigure[i].geometry.vertices );
-                arrayFigure[i].points = new THREE.Points( arrayFigure[i].pointsGeometry, arrayFigure[i].pointsMaterial );
-                arrayFigure[i].scene.add( arrayFigure[i].points );
+                arrayFigure[i].light = new THREE.DirectionalLight( 0xffffff, 1);
+                arrayFigure[i].light.position.set(100, 100, 100);
+                arrayFigure[i].scene.add(arrayFigure[i].light);
+                arrayFigure[i].ambLight = new THREE.AmbientLight( 0x909090);
+                arrayFigure[i].scene.add(arrayFigure[i].ambLight);
 
-                arrayFigure[i].material = new THREE.MeshLambertMaterial( {
-                    color: 0xffffff,
-                    opacity: 0.1,
-                    transparent: true
-                } );
+                arrayFigure[i].dodecahedronGeom = new THREE.DodecahedronGeometry(10);
+                arrayFigure[i].dodecahedron = new THREE.Mesh(arrayFigure[i].dodecahedronGeom, new THREE.MeshLambertMaterial({color: "white", transparent: true, opacity: .75}));
+                arrayFigure[i].scene.add(arrayFigure[i].dodecahedron);
+                arrayFigure[i].edgesGeom = new THREE.EdgesGeometry(arrayFigure[i].dodecahedronGeom);
+                console.log(arrayFigure[i].edgesGeom);
+                arrayFigure[i].edges = new THREE.LineSegments(arrayFigure[i].edgesGeom, new THREE.LineBasicMaterial({color: "red"}));
+                arrayFigure[i].scene.add(arrayFigure[i].edges);
 
-                arrayFigure[i].geometry = new THREE.ConvexBufferGeometry( arrayFigure[i].geometry.vertices );
+                arrayFigure[i].thickness = 0.25;
 
-                arrayFigure[i].mesh = new THREE.Mesh(arrayFigure[i].geometry, arrayFigure[i].material);
-                arrayFigure[i].mesh.material.side = THREE.BackSide;
-                arrayFigure[i].mesh.renderOrder = 0;
-                arrayFigure[i].scene.add( arrayFigure[i].mesh );
+                for (var l = 0; l < arrayFigure[i].edgesGeom.attributes.position.count; l++){
+                    arrayFigure[i].sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(arrayFigure[i].thickness * 2, 16,8), new THREE.MeshStandardMaterial({color: "red"}));
+                    arrayFigure[i].sphere.position.set(
+                        arrayFigure[i].edgesGeom.attributes.position.array[(l * 3) + 0],
+                        arrayFigure[i].edgesGeom.attributes.position.array[(l * 3) + 1],
+                        arrayFigure[i].edgesGeom.attributes.position.array[(l * 3) + 2]
+                    );
+                    arrayFigure[i].scene.add(arrayFigure[i].sphere);
+                }
 
-                arrayFigure[i].mesh = new THREE.Mesh( arrayFigure[i].geometry, arrayFigure[i].material.clone() );
-                arrayFigure[i].mesh.material.side = THREE.FrontSide;
-                arrayFigure[i].mesh.renderOrder = 1;
-                arrayFigure[i].scene.add( arrayFigure[i].mesh );
+                for (var k = 0; k < arrayFigure[i].edgesGeom.attributes.position.count - 1; k+=2){
+
+                    arrayFigure[i].startPoint = new THREE.Vector3(
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 0],
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 1],
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 2]
+                    );
+                    arrayFigure[i].endPoint = new THREE.Vector3(
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 3],
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 4],
+                        arrayFigure[i].edgesGeom.attributes.position.array[k * 3 + 5]
+                    );
+
+                    arrayFigure[i].cylLength = new THREE.Vector3().subVectors(arrayFigure[i].endPoint, arrayFigure[i].startPoint).length();
+                    arrayFigure[i].cylGeom = new THREE.CylinderBufferGeometry(arrayFigure[i].thickness, arrayFigure[i].thickness, arrayFigure[i].cylLength, 16);
+                    arrayFigure[i].cylGeom.translate(0, arrayFigure[i].cylLength / 2, 0);
+                    arrayFigure[i].cylGeom.rotateX(Math.PI / 2);
+                    arrayFigure[i].cyl = new THREE.Mesh(arrayFigure[i].cylGeom, new THREE.MeshLambertMaterial({color: "blue"}));
+                    arrayFigure[i].cyl.position.copy(arrayFigure[i].startPoint);
+                    arrayFigure[i].cyl.lookAt(arrayFigure[i].endPoint);
+                    arrayFigure[i].scene.add(arrayFigure[i].cyl);
+                }
+
 
                 break;
 
             case 'poly':
 
-                arrayFigure[i].geometry = new THREE.IcosahedronGeometry(100);
+                // arrayFigure[i].geometry = new THREE.IcosahedronGeometry(100);
 
                 arrayFigure[i].scene = new THREE.Scene();
                 arrayFigure[i].scene.background = new THREE.Color( bgColorFigure );
                 arrayFigure[i].camera = new THREE.PerspectiveCamera(45, widthFigure/heightFigure , 0.1, 1000);
-                arrayFigure[i].camera.position.z = 300;
+                arrayFigure[i].camera.position.z = 600;
                 arrayFigure[i].renderer = new THREE.WebGLRenderer();
                 arrayFigure[i].renderer.setSize( widthFigure - paddingFigure*2 ,heightFigure - paddingFigure*2);
                 arrayFigure[i].container = document.getElementById(arrayFigure[i].name);
                 arrayFigure[i].container.appendChild( arrayFigure[i].renderer.domElement );
 
-                arrayFigure[i].material =
-                    new THREE.MeshBasicMaterial({
-                        color: 0x888888,
-                        wireframe: true
-                    });
+                arrayFigure[i].geometry = new THREE.PlaneGeometry(200, 200, 200,100);
+
+                arrayFigure[i].material = new THREE.MeshLambertMaterial({color:0xff0000,opacity:0.2,transparent:true,overdraw:0.5});
+
+                // arrayFigure[i].material =
+                //     new THREE.MeshBasicMaterial({
+                //         color: 0x888888,
+                //         wireframe: true
+                //     });
+                //
 
                 arrayFigure[i].mesh = new THREE.Mesh(arrayFigure[i].geometry, arrayFigure[i].material);
 
@@ -226,8 +305,8 @@ function func_threejs_animation() {
                 arrayFigure[i].scene.rotation.y += arrayFigure[i].mouseX*0.00008;
             }
             if( arrayFigure[i].name == 'poly' ){
-                arrayFigure[i].mesh.rotation.x += arrayFigure[i].mouseY*0.00008;
-                arrayFigure[i].mesh.rotation.y += arrayFigure[i].mouseX*0.00008;
+                arrayFigure[i].scene.rotation.x += arrayFigure[i].mouseY*0.00008;
+                arrayFigure[i].scene.rotation.y += arrayFigure[i].mouseX*0.00008;
             }
             if( arrayFigure[i].name == 'cube' ){
                 arrayFigure[i].scene.rotation.x += arrayFigure[i].mouseY*0.00008;
@@ -241,9 +320,9 @@ function func_threejs_animation() {
                 arrayFigure[i].scene.rotation.z += 0.0025;
             }
             if( arrayFigure[i].name == 'poly' ){
-                arrayFigure[i].mesh.rotation.x += 0.0025;
-                arrayFigure[i].mesh.rotation.y += 0.0025;
-                arrayFigure[i].mesh.rotation.z += 0.0025;
+                arrayFigure[i].scene.rotation.x += 0.0025;
+                arrayFigure[i].scene.rotation.y += 0.0025;
+                arrayFigure[i].scene.rotation.z += 0.0025;
             }
             if( arrayFigure[i].name == 'cube' ){
                 arrayFigure[i].scene.rotation.x += 0.0025;
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,3 +1,4 @@
 <script src="{{ mix( 'js/all.js' ) }}"></script>
+<script src="https://threejs.org/examples/js/controls/OrbitControls.js"></script>
 
 @yield('scripts')
